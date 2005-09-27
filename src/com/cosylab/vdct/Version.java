17ace31a6b14e5d6339dcaba8e9c631612e37605
Matej Sekoranja <matej.sekoranja@cosylab.com>
2005-09-27 13:25:41 +0000

    build++

--- src/com/cosylab/vdct/Version.java
+++ src/com/cosylab/vdct/Version.java
@@ -36,7 +36,7 @@ package com.cosylab.vdct;
  */
 public interface Version {
 	public final static String VERSION = "2.5";
-	public final static String BUILD = "1264";
+	public final static String BUILD = "1265";
 
 	public final static String JAVA_VERSION_REQUIRED = "1.4.0";
 }
